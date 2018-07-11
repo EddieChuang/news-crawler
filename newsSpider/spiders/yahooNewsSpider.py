ab206bd87dbcd039ccf5be2a2f97bf09e408321d
chiamin <chiamin@appledeMacBook-Pro.local>
2018-07-11 23:47:54 +0800

    CrawlSpider with SplashRequest

--- newsSpider/spiders/yahooNewsSpider.py
+++ newsSpider/spiders/yahooNewsSpider.py
@@ -7,42 +7,29 @@ from scrapy_splash import SplashRequest
 class YahooNewsSpider(CrawlSpider):
     name = 'yahoo'
 
-    # allowed_domains = ['news.cnyes.com']
-    # start_urls = ['https://news.cnyes.com/news/cat/headline']
-    # rules = [Rule(LinkExtractor(allow=['/news/id/'], deny=['\/print']), 'parse_news', follow=True)]
-    # def start_requests(self):
-    #     urls = ['https://tw.money.yahoo.com/']
-    #     for url in urls:
-    #         yield scrapy.Request(url=url, callback=self.parse)
-    #
-    # def parse(self, response):
-    #     print(response.url)
-    #     # print(response.body)
-    #
-    #     with open('money.html', 'wb') as file:
-    #         file.write(response.body)
-
-
-
-
     # scrapy + splash
 
     allowed_domains = ['tw.news.yahoo.com']
     start_urls = ['https://tw.news.yahoo.com']
     # start_urls = ["https://tw.news.yahoo.com/technology"]
+    # start_urls = ['https://tw.news.yahoo.com/tech-development']
+
+    # rules = [
+    #     Rule(LinkExtractor(allow='\.html'), callback='parse_result', process_request="use_splash")
+    # ]
 
     def start_requests(self):
-        # lua
+        # lua script
         script = """
             function main(splash, args)
-                local num_scrolls = 1
+                local num_scrolls = 10
                 local scroll_delay = 1.0
                 local scroll_to = splash:jsfunc("window.scrollTo")
                 local get_body_height = splash:jsfunc("function() {return document.body.scrollHeight}")
-                
+
                 assert(splash:go(args.url))
                 splash:wait(splash.args.wait)
-                
+
                 for _ = 1, num_scrolls do
                     scroll_to(0, get_body_height())
                     splash:wait(scroll_delay)
@@ -51,24 +38,58 @@ class YahooNewsSpider(CrawlSpider):
             end
         """
         # end of lua
-
         splash_args = {'lua_source': script, 'wait': 2}
         for url in self.start_urls:
-            yield SplashRequest(url, self.parse_result, endpoint='execute', args=splash_args)
+            yield SplashRequest(url, dont_process_response=True, args=splash_args, endpoint='execute', meta={'real_url': url})
+
+        rules = [Rule(LinkExtractor(allow='[.]*'), callback='parse_result', process_request="splash_request")]
+
+    def splash_request(self, request):
+        # lua script
+        script = """
+            function main(splash, args)
+                local num_scrolls = 10
+                local scroll_delay = 1.0
+                local scroll_to = splash:jsfunc("window.scrollTo")
+                local get_body_height = splash:jsfunc("function() {return document.body.scrollHeight}")
+
+                assert(splash:go(args.url))
+                splash:wait(splash.args.wait)
+
+                for _ = 1, num_scrolls do
+                    scroll_to(0, get_body_height())
+                    splash:wait(scroll_delay)
+                end 
+                return splash:html()
+            end
+        """
+        # end of lua
+        splash_args = {'lua_source': script, 'wait': 2}
+        return SplashRequest(url=request.url, dont_process_response=True, endpoint='execute', args=splash_args,
+                             meta={'real_url': request.url})
+
+    def _requests_to_follow(self, response):
+
+        seen = set()
+        new_response = response.replace(url=response.meta.get('real_url'))
+        for n, rule in enumerate(self.rules):
+            links = [link for link in rule.link_extractor.extract_links(new_response) if link not in seen]
+            if links and rule.process_links:
+                links = rule.process_links(links)
+                for link in links:
+                    seen.add(link)
+                    r = self._build_request(n, link)
+                    yield rule.process_links(r)
+
 
     def parse_result(self, response):
-        print(response)
+        print(response.url)
 
-        LINK_SELECTOR = 'a'
-        # LINK_SELECTOR = 'h3.MB\(5px\) a ::attr(href)'
-        links = response.css(LINK_SELECTOR)
-
-        for link in links:
-            print(link)
-
-
-
-
+        # LINK_SELECTOR = 'h3 a::attr(href)'
+        # links = response.css(LINK_SELECTOR).extract()
+        #
+        # for link in links:
+        #     print(link)
 
 # import requests
 #
